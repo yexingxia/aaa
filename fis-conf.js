f497e121732e8c6999290f2b46682a1aae5a0b3f
wmf <wayne@bl06260.800best.net>
2016-09-29 17:06:44 +0800

    add deploy && some adjustments

--- fis-conf.js
+++ fis-conf.js
@@ -4,65 +4,76 @@
 fis.hook('commonjs');
 
 fis.match('::package', {
-  postpackager: fis.plugin('loader', {})
-});
-
-fis.match('*.less', {
-  parser: fis.plugin('less-2.x'),
-  rExt: '.css'
-});
-
-fis.match('*.{js,css}', {
-  useHash: true
-});
-
-fis.match('::image', {
-  useHash: true
-});
-
-fis.match('*.png', {
-  // fis-optimizer-png-compressor 插件进行压缩，已内置
-  optimizer: fis.plugin('png-compressor', {
-  	type: 'pngquant' //default is pngcrush
-  })
-});
-
-fis.match('/widget/**.js', {
-  isMod: true
-});
-
-
-
+		postpackager: fis.plugin('loader', {})
+	})
+	.match('*.html', {
+		optimizer: fis.plugin('html-minifier')
+	})
+	.match('::image', {
+		useHash: true
+	})
+	.match('*.png', {
+		// fis-optimizer-png-compressor 插件进行压缩，已内置
+		optimizer: fis.plugin('png-compressor', {
+			type: 'pngquant' //default is pngcrush
+		})
+	})
+	.match('*.ico', {
+		useHash: false
+	})
+	.match('/widget/**.js', {
+		isMod: true,
+		useHash: false
+	})
+	.match('*.less', {
+		parser: fis.plugin('less-2.x'),
+		rExt: '.css'
+	});
 // Global end
 
-// default media is `dev`
+// default media `dev`
 fis.media('dev')
 	.match('*', {
 		useHash: false,
 		optimizer: null
 	});
 
-// extends GLOBAL config
+// media `prod`
 fis.media('prod')
-	.match('*.{less,css}', {
-	  packTo: '/static/aio.css'
+	.match('/{pkg,static,page}/**', {
+		deploy: fis.plugin('http-push', {
+			receiver: 'http://10.45.25.82:8999/receiver',
+			//远端目录
+			to: '/deploy'
+		})
 	})
-	.match('*.html', {
-		optimizer: fis.plugin('html-minifier')
+	.match('/favicon.ico', {
+		deploy: fis.plugin('http-push', {
+			receiver: 'http://10.45.25.82:8999/receiver',
+			//远端目录
+			to: '/deploy'
+		})
+	})
+	.match('/static/*', {
+		deploy: fis.plugin('http-push', {
+			receiver: 'http://10.45.25.82:8999/receiver',
+			//远端目录
+			to: '/deploy'
+		})
 	})
 	.match('/widget/**.js', {
-	  	optimizer: fis.plugin('uglify-js'),
-	  	exclude: '/widget/ui/**.js',
-	  	packTo: '/static/aio.js'
+		optimizer: fis.plugin('uglify-js'),
+		exclude: '/widget/ui/**.js',
+		packTo: '/pkg/aio.js'
 	})
 	.match('/widget/ui/**.js', {
 		optimizer: fis.plugin('uglify-js'),
-		packTo: '/static/common.js'
+		packTo: '/pkg/common.js'
 	})
-	.match('*.css', {
-	  	optimizer: fis.plugin('clean-css'),
-	  	packTo: '/static/aio.css'
+	.match('*.{less,css}', {
+		optimizer: fis.plugin('clean-css'),
+		packTo: '/pkg/aio.css'
 	})
-	.match('*.png', {
-	  	optimizer: fis.plugin('png-compressor')
+	.match('/pkg/*', {
+		useHash: true
 	});
--- widget/hao123/hao123.tpl
+++ widget/hao123/hao123.tpl
@@ -5,7 +5,7 @@
 <div class="wrapper">
 	<div class="mod-hao123">
 		<div class="toolbar">
-			<a class="add-desktop" href="/widget/hao123/hao123.url" download="百世快递123.url">添加到桌面</a>
+			<a class="add-desktop" href="/static/百世快递123.url" download="百世快递123.url">下载快捷方式</a>
 			<a class="add-favorite" href="#">收藏</a>
 		</div>
 		<h1 class="title">Best123</h1>
